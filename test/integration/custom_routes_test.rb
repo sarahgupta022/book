7cd2423b29c51673986267a79fc415317c00fcce
sarah gupta <sarahgupta022@gmail.com>
2015-05-06 17:48:52 -0700

    Added tests for custom routes.

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+test "that /login route opens the login page" do
+get '/login'
+assert_response :success
+    end
+    
+    test "that /login route opens the login page" do
+      get '/logout'
+      assert_response :redirect
+      assert_redirected_to '/'
+    end
+end
