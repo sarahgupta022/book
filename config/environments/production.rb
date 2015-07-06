8d00257d1f0ff1b04acf453eac318f12a14e9ce4
sarah gupta <sarahgupta022@gmail.com>
2015-07-06 14:38:14 -0700

    Updated config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -80,13 +80,14 @@ Rails.application.configure do
   
   
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'friendsbook1.herokuapp.com',
   user_name:            'sarahgupta022@gmail.com',
   password:             'arvind@22',
-  authentication:       'plain',
+  authentication:       :plain,
   enable_starttls_auto: true  }
   
 end
